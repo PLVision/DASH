cd94d0e9b8ce7bd7cd46705cba9d036d8dce41e0
elchupanebrej <kostyagoloveshko@gmail.com>
2022-09-05 07:14:49 +0300

    Integration fixes

--- test/test-cases/test_vector_example/test_vnet_inbound.py
+++ test/test-cases/test_vector_example/test_vnet_inbound.py
@@ -8,9 +8,9 @@ def test_vnet_inbound(dpu, dataplane):
     with (current_file_dir / 'test_vnet_inbound_setup_commands.json').open(mode='r') as config_file:
         vnet_inbound_setup_commands = json.load(config_file)
 
-    dpu.process_commands(vnet_inbound_setup_commands)
+    result = [*dpu.process_commands(vnet_inbound_setup_commands)]
 
     with (current_file_dir / 'test_vnet_inbound_cleanup_commands.json').open(mode='r') as config_file:
         vnet_inbound_cleanup_commands = json.load(config_file)
 
-    dpu.process_commands(vnet_inbound_cleanup_commands)
+    result = [*dpu.process_commands(vnet_inbound_cleanup_commands)]
